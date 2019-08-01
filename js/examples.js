a81d8bb5fccc6b1532c4224a84c2d4adc0345310
MisterMoody <principal@mistermoody.com>
2019-08-01 18:29:31 -0400

    Arranged Function Examples in a Logical Order

--- js/examples.js
+++ js/examples.js
@@ -1,4 +1,4 @@
-// Alert Button Notification -----------------------------------
+// EX#1 Alert Button Notification -----------------------------------
 const alertBTN = document.getElementById('alertBTN');
 alertBTN.addEventListener('click', () => {
   alert('This is a user notification. Add content here to display a useful message notifiying the user of important information.')
@@ -6,13 +6,28 @@ alertBTN.addEventListener('click', () => {
 // -------------------------------------------------------------
 
 
-// Tooltip Description -----------------------------------------
+// EX#2 Tooltip Description -----------------------------------------
 const tooltip = document.getElementById('tooltip');
 tooltip.title = "the quality or condition of being specific";
 // -------------------------------------------------------------
 
 
-// Toggle Element (on Click) -----------------------------------
+// EX#3 Hover Manipulation ------------------------------------------
+const hoverList = document.querySelector('.hoverList');
+hoverList.addEventListener('mouseover', (event) => {
+  if (event.target.tagName == 'P') {
+    event.target.textContent = event.target.textContent.toUpperCase();
+  }
+});
+hoverList.addEventListener('mouseout', (event) => {
+  if (event.target.tagName == 'P') {
+    event.target.textContent = event.target.textContent.toLowerCase();
+  }
+});
+// -------------------------------------------------------------
+
+
+// EX#4 Toggle Element (on Click) -----------------------------------
 const toggleList = document.getElementById('toggleList');
 const listDiv = document.querySelector('.list');
 
@@ -31,58 +46,34 @@ toggleList.addEventListener('click', () => {
 // -------------------------------------------------------------
 
 
-// Hover Manipulation ------------------------------------------
-const hoverList = document.querySelector('.hoverList');
-hoverList.addEventListener('mouseover', (event) => {
-  if (event.target.tagName == 'P') {
-    event.target.textContent = event.target.textContent.toUpperCase();
-  }
-});
-hoverList.addEventListener('mouseout', (event) => {
-  if (event.target.tagName == 'P') {
-    event.target.textContent = event.target.textContent.toLowerCase();
-  }
-});
-// -------------------------------------------------------------
 
-
-// Input Manipulation ------------------------------------------
-const p = document.querySelector('p.description');
-const modifyInput = document.querySelector('input.description');
-const modifyBTN = document.querySelector('button.description');
-//  Listen for User-Interaction
-//  Calls .addEventListener(); Method on Button
-modifyBTN.addEventListener('click', () => {
-  //  'click' will use "value of" <input> to 'Set <p>
-  p.textContent = modifyInput.value;
-});
-// -------------------------------------------------------------
-
-
-// CODE WORKS (Issues Rendereing due to <tag> Conflicts elsewhere IN THIS SCRIPT!
-///  Add + Remove Elements with Buttons ----------
-const addItemInput = document.querySelector('input.addItemInput');
-const addItemButton = document.querySelector('button.addItemButton');
-const removeItemButton = document.querySelector('button.removeItemButton');
-
-addItemButton.addEventListener('click', () => {
-  let ul = document.getElementsByTagName('ul')[0];
-  let li = document.createElement('li');
-  li.textContent = addItemInput.value;
-  ul.appendChild(li);
-  addItemInput.value = '';
-});
-removeItemButton.addEventListener('click', () => {
-  let ul = document.getElementsByTagName('ul')[0];
-  let li = document.querySelector('li:last-child');
-  ul.removeChild(li);
-});
-// ----------------------------------------------
+/*  EX#5 STYLING <elements> */
+//  SELECT (Reference) an Element
+const myList = document.getElementsByClassName('purple')
+//  Function to 'Iterate over all <li>'
+  //  Makes color for all <li> 'purple'
+for (let i = 0; i < myList.length; i += 1) {
+  myList[i].style.color = 'purple';
+}
+//  SELECT (Reference) an Element
+const notPurple = document.getElementsByClassName('not-purple')
+//  Function to 'Iterate over <li> with class="not-purple" '
+  //  Makes color 'red' for <li> with the class 'not-purple'
+for (let i = 0; i < notPurple.length; i += 1) {
+  notPurple[i].style.color = 'red';
+}
+//  SELECT (Reference) an Element
+const evens = document.querySelectorAll('.table > li:nth-child(even)');
+//  Function to 'Iterate over all <li>'
+  //  Makes the background color for every-other <li> 'lightgray'
+for (let i = 0; i < evens.length; i += 1) {
+  evens[i].style.backgroundColor = 'lightgray';
+}
+////////////////////////////////////////////////////////////////
 
 
 
-
-/*  EXAMPLE 1: MODIFY CONTENT using HTMLElement.style Property  */
+/*  EX#6 MODIFY CONTENT using HTMLElement.style Property  */
 //  SELECT (Reference) an Element
 const textStyles = document.getElementById('textStyles');
 //  Event Listener to 'Listen for' Mouse-Clicks
@@ -93,10 +84,8 @@ textStyles.addEventListener('click', () => {
   textStyles.style.border = '2px dashed red';  // Set Border 
   textStyles.style.textAlign = 'center';        // Set Text-Alignment
 });
-////////////////////////////////////////////////////////////////////////
-
-
-/*  EXAMPLE 2: RESET Modified Content */
+      /*      ++++++++       */
+/*  EX#6 RESET Modified Content */
 //  SELECT (Reference) an Element
 const textStyleZ = document.getElementById('textStyleZ');
 const resetButton = document.getElementById('resetButton');
@@ -114,7 +103,7 @@ resetButton.addEventListener('click', () => {
 ////////////////////////////////////////////////////////////////////////
 
 
-/*  EXAMPLE 3:  Use 'Input' to Modify Content */
+/*  EX#7 Use 'Input' to Modify Content */
 //  SELECT (Reference) an Element
 const textInputStyle = document.getElementById('textInputStyle');
 const inputButton = document.getElementById('inputButton');
@@ -128,28 +117,35 @@ inputButton.addEventListener('click', () => {
 ////////////////////////////////////////////////////////////////////////
 
 
-/*  EXAMPLE 4:   */
-//  SELECT (Reference) an Element
-const myList = document.getElementsByTagName('li')
-//  Function to 'Iterate over all <li>'
-  //  Makes color for all <li> 'purple'
-for (let i = 0; i < myList.length; i += 1) {
-  myList[i].style.color = 'purple';
-}
-//  SELECT (Reference) an Element
-const notPurple = document.getElementsByClassName('not-purple')
-//  Function to 'Iterate over <li> with class="not-purple" '
-  //  Makes color 'red' for <li> with the class 'not-purple'
-for (let i = 0; i < notPurple.length; i += 1) {
-  notPurple[i].style.color = 'red';
-}
-//  SELECT (Reference) an Element
-const evens = document.querySelectorAll('li:nth-child(even)');
-//  Function to 'Iterate over all <li>'
-  //  Makes the background color for every-other <li> 'lightgray'
-for (let i = 0; i < evens.length; i += 1) {
-  evens[i].style.backgroundColor = 'lightgray';
-}
-////////////////////////////////////////////////////////////////
+// EX#8 Input Manipulation ------------------------------------------
+const p = document.querySelector('p.description');
+const modifyInput = document.querySelector('input.description');
+const modifyBTN = document.querySelector('button.description');
+//  Listen for User-Interaction
+//  Calls .addEventListener(); Method on Button
+modifyBTN.addEventListener('click', () => {
+  //  'click' will use "value of" <input> to 'Set <p>
+  p.textContent = modifyInput.value;
+});
+// -------------------------------------------------------------
 
 
+//  EX#9 Add + Remove Elements with Buttons ----------
+// CODE WORKS (Issues Rendereing due to <tag> Conflicts elsewhere IN THIS SCRIPT!
+const addItemInput = document.querySelector('input.addItemInput');
+const addItemButton = document.querySelector('button.addItemButton');
+const removeItemButton = document.querySelector('button.removeItemButton');
+
+addItemButton.addEventListener('click', () => {
+  let ul = document.getElementsByTagName('ul')[0];
+  let li = document.createElement('li');
+  li.textContent = addItemInput.value;
+  ul.appendChild(li);
+  addItemInput.value = '';
+});
+removeItemButton.addEventListener('click', () => {
+  let ul = document.getElementsByTagName('ul')[0];
+  let li = document.querySelector('li:last-child');
+  ul.removeChild(li);
+});
+// ----------------------------------------------
